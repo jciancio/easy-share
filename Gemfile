6c989918bcec28482ed5b3c99dd8e45f45b4a551
jciancio <jciancio7@gmail.com>
2017-01-22 12:58:50 -0500

    set up uploader with aws

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
 gem 'devise'
-gem 'carrierwave'
+gem 'carrierwave', github: 'carrierwaveuploader/carrierwave'
 gem 'mini_magick'
 gem 'fog'
+gem 'figaro'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :user_events
   has_many :events, through: :user_events
 
+  validates_integrity_of :avatar
+  validates_processing_of :avatar
+
   def to_s
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def avatar_size_validation
+    errors[:avatar] << "should be less thank 500kb" if avatar.size > 0.5.megabytes
   end
 end
